1c163050b01b6d1bdf65e20718755da2063ba322
uos-datavisdashboard <uchekesla@gmail.com>
2022-10-13 16:35:48 +0100

    make it better

--- Context/speaker.py
+++ Context/speaker.py
@@ -25,8 +25,6 @@ GOOGLE_TASKS_TASKLISTS = {
 month = datetime.datetime.now().strftime("%m")
 day= datetime.datetime.now().strftime("%d")
 
-print(month)
-print(day)
 
 class SoftwareInteligenzaArtificiale(object):
 
